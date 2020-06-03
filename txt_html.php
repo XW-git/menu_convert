7ed5384fcef5a6aa61181439a8939a2174e76b8f
XW-git <63622661+XW-git@users.noreply.github.com>
2020-06-03 11:37:03 -0700

    simple out put file( already html format )
    <tt>Chef Special Dinners</tt>
    <tt_ds>(Served with Miso Soup & House Salad) Sushi/Sashimi is Pre-Selected by Chef</tt_ds>
    
    Sushi Regular *<pr>17.00</pr>
    <ds>(California Maki and 8 pcs Sushi) </ds>
    
    Sushi Deluxe *<pr>20.25</pr>
    <ds>(Spicy Tuna Maki and 10 pcs Sushi) </ds>
    
    Sashimi Deluxe *<pr>23.75</pr>
    <ds>(Assortment of 5 different Sashimi & 15 pcs total) </ds>
    
    Sushi *<pr>40.00</pr>
    <ds>(for 2) (Chef Special Maki, California Maki, Tuna Maki,& 12 pcs Sushi) </ds>
    
    Sushi Sashimi *<pr>59.00</pr>
    <ds>(for 2) (California Maki, Chef Special Maki,10 pcs Sushi & 15 pcs Sashimi) </ds>
    
    Chirashi Deluxe *<pr>25.00</pr>
    <ds>(13 pcs Sashimi on top of Sushi Rice) </ds>

--- txt_html.php
+++ txt_html.php
@@ -20,9 +20,22 @@
   color:  #df4623;
   text-align: center;
   font-family:sail;
+  line-height: 28pt
+ 
+}
+.menu_css tt_ds {         
+  display: block;
+  text-transform: none;
+  font-size: 15px;
+  padding: 0 20px 10px;
+  line-height: 12px;
+  font-style: normal;
+  color: white;
+  font-family:corbel;
+  text-align: center;
+   line-height: 0pt 
   
 }
-
 .menu_css ds {         
   display: block;
   text-transform: none;
@@ -47,41 +60,13 @@
 
 <?php
 
-$test_csv =  ABSPATH.'restaurant_menu/a.txt';
+$file_name =  ABSPATH.'restaurant_menu/b.txt';
 
-$i = 0;
-$lines = file($test_csv);
+$lines = file($file_name);
 
 foreach ($lines as $line_num => $line) {
-$str=$line;
-    if (bin2hex($line)=="0d0a"){
-    // new item starts
-    $i = 0;
-    }
-    else{
-    $i = $i+1;
-    }
-      
-    switch ($i) {
-        case 0:
-            break;
-        case 1: 
-            $price=strrchr($line,"\t"); 
-            if($price==""){
-                echo "<tt>". $line. "</tt>";
-            }
-            else{ 
-                $dish_name=str_replace($price,"",$line);
-                echo  $dish_name;
-                echo  "<pr>". $price ."</pr>". "<br>";
-            }            
-            break;
-       case 2:
-            echo "<ds>". $line. "</ds>";
-            break;
-       default:
-            break; 
-    } 
+
+echo $line;
 }
 
 ?>
